1df52ebf4d9f9058545785be3baed3db439624a1
kavitha9412 <kichenassamy@et.esiea.fr>
2021-12-05 22:38:42 +0100

    Messages de log

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,9 +1,13 @@
 package fr.lernejo.logger;
 
+
+import java.util.function.Predicate;
+
 public class LoggerFactory {
 
     public static Logger getLogger(String name){
 
+        Predicate<String> condition = message -> message.contains("simulation");
         return new ConsoleLogger();
     }
 }
