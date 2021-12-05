b396d47550f00bed86013de1a41a72686423147c
kavitha9412 <kichenassamy@et.esiea.fr>
2021-12-05 22:27:28 +0100

    Création d'un utilisateur robot

--- src/main/java/fr/lernejo/guessgame/Launcher.java
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -6,16 +6,37 @@ import java.security.SecureRandom;
 
 public class Launcher {
     public static void main(String[] args) {
-    Player player = new HumanPlayer();
-    Simulation simulation = new Simulation(player);
 
-    SecureRandom random = new SecureRandom();
-    long randomNumber = random.nextInt(100);
+        Logger l =new ConsoleLogger();
 
-    simulation.initialize(randomNumber);
-    simulation.loopUntilPlayerSucceed(randomNumber);
+        if(args.length==1 && args[0].equals("-interactive"))
+        {
+
+            SecureRandom random = new SecureRandom();
+            long randomNumber = random.nextInt(100); // génère un nombre entre 0 (inclus) et 100 (exclus)
+            Player player = new HumanPlayer();
+            Simulation sim = new Simulation(player);
+            sim.initialize(randomNumber);
+            sim.loopUntilPlayerSucceed( Long.MAX_VALUE);
+        }
+        else if (args.length == 2 && (args[0].equals("-auto") && args[1].matches("[0-9]+")))
+        {
+            Player computerplayer = new ComputerPlayer();
+            Simulation sim = new Simulation(computerplayer);
+            sim.initialize(Long.parseLong(args[1]));
+            sim.loopUntilPlayerSucceed(1000);
+        }
+        else
+            l.log("Erreur Argument :\n -interactive => partie interactive\n -auto [0-9] => partie robot");
+
+
+
 
     }
+
+
 }
+
+
 
 
